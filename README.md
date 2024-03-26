243bed052adaaf315840e54e91e828d1752ae65e
Francis Lim <thyeem@gmail.com>
2024-03-27 03:48:01 +0900

    bump up to v0.1.0

--- README.md
+++ README.md
@@ -1,50 +1,51 @@
-# s-LISP
+# sLISP
 
-`SLISP` is a pure-functional `LISP` implementation in `Haskell`.
-Initially, it started as a simple `S-expr` parser to demonstrate how the [parser-S](https://github.com/thyeem/s) nicely works, but decided to go a little more.
+`sLISP` is a pure-functional `LISP` implementation in `Haskell`.
+Initially, it started as a simple _S-expression_ parser to demonstrate how the [`s`](https://github.com/thyeem/s) nicely works, but decided to go a little more.
 
-I hope that the 1k+ lines of this code will be one of the simplest and the most readable LISP implementation, compared to its functionality.
+I hope that this **1k+ lines of code** will be one of the simplest and _the most readable LISP implementation_ compared to capability it has.
 
 ## Features
-  - Based on [parser-S](https://github.com/thyeem/s) (plays all roles in the `R` part in the REPL)
+  - Based on [`s`](https://github.com/thyeem/s) (plays all roles in the `R` part in the REPL)
   - Pure-functional (_NO reference object used like `IORef`_)
-  - Coded with a very few principles and patterns -> the concept of a state machine (each bind `>>=` exactly indicates a single state transition)
-  - Compatible with a few hundreds of `CLISP`'s main built-in functions (on-progress)
-  - Implemented from scratch using _Haskell_ `prelude` only
+  - Coded with a very few principles and patterns 
+    > each bind (`>>=`) exactly corresponds to a single state transition
+  - Almost compatible with `CLISP` main built-in functions (in progress)
 
+## Build
+```bash
+$ git clone https://github.com/thyeem/slisp
+
+# build: this yields ~80kB binary 'sl'
+$ stack build
+
+# run sLISP REPL
+$ stack run
+
+# test
+$ stack test
+
+# documentation
+$ stack haddock --open slisp 
+```
 
 ## Demo
-<img src="demo.gif" width="540" />
+<img src="demo.gif" width="480" />
 
 ## REPL
-`SLISP` __REPL__ supports simple but some useful modes. Each mode is switched right after key input as shown below.
+`sLISP` __REPL__ supports simple but some useful modes. Each mode is switched right after key input as shown below.
 
 ```lisp
 ;; one semicolon -> paste-mode (multi-line input)
-SLISP> ;
+sLISP> ;
 
 ;; two semicolons -> debug-mode (describe S-expression with hierarchical structure)
-SLISP> ;;
+sLISP> ;;
 
 ;; three semicolons -> view the REPL environment (symbol-data map)
-SLISP> ;;;
+sLISP> ;;;
 
 ;; four semicolons -> list all built-in functions
-SLISP> ;;;;
+sLISP> ;;;;
 
-```
-## Build
-```bash
-# Assume the Haskell GHC and cabal is installed
-
-# build: this yields ~80kB binary 'sl' in ./app directory.
-$ make release
-
-# test
-$ make test
-
-# documentation
-$ make doc
-
-$ make opendoc
 ```
--- test/SLispSpec.hs
+++ test/SLispSpec.hs
@@ -1,17 +1,17 @@
 module SLispSpec where
 
-import           SLisp
-import           Test.Hspec
+import SLisp
+import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "SLISP REPL-show" $ do
+  describe "sLISP REPL show" $ do
     mapM_ (uncurry (test t)) in'out
 
 t :: String -> String
 t stream = do
   case re stream (init'env 0) of
-    Left  err    -> err
+    Left err -> err
     Right (_, e) -> show' e
 
 test :: (String -> String) -> String -> String -> SpecWith (Arg Expectation)
@@ -20,73 +20,76 @@ test f i o = it i $ do
 
 in'out :: [(String, String)]
 in'out =
-  [ ("nil"                             , "nil")
-  , ("'nil"                            , "nil")
-  , ("`nil"                            , "nil")
-  , ("()"                              , "nil")
-  , ("'()"                             , "nil")
-  , ("`()"                             , "nil")
+  [ ("nil", "nil")
+  , ("'nil", "nil")
+  , ("`nil", "nil")
+  , ("()", "nil")
+  , ("'()", "nil")
+  , ("`()", "nil")
   , ("(eval (nth 2 '(1 2 (+ (* 34 43) 22) 4 5)))", "1484")
-  , ("(cons '(2 . 3) ())"              , "((2 . 3))")
-  , ("(= 1 1 1)"                       , "t")
-  , ("(= 1 1 2)"                       , "nil")
-  , ("(< 1 2 3)"                       , "t")
-  , ("(< 1 3 2)"                       , "nil")
-  , ("(list 1 2 3)"                    , "(1 2 3)")
-  , ("(cons 1 2)"                      , "(1 . 2)")
-  , ("(car (cons 1 2))"                , "1")
-  , ("(cdr (cons 1 2))"                , "2")
-  , ("(first '(1 2))"                  , "1")
-  , ("(second '(1 2))"                 , "2")
-  , ("(defvar covid-19 2020)"          , "covid-19")
-  , ("(setq covid-19 2020)"            , "2020")
-  , ("(let () (defparameter x 1) x)"   , "1")
-  , ("(let () (setq y 'covid-19) y)"   , "covid-19")
+  , ("(cons '(2 . 3) ())", "((2 . 3))")
+  , ("(= 1 1 1)", "t")
+  , ("(= 1 1 2)", "nil")
+  , ("(< 1 2 3)", "t")
+  , ("(< 1 3 2)", "nil")
+  , ("(list 1 2 3)", "(1 2 3)")
+  , ("(cons 1 2)", "(1 . 2)")
+  , ("(car (cons 1 2))", "1")
+  , ("(cdr (cons 1 2))", "2")
+  , ("(first '(1 2))", "1")
+  , ("(second '(1 2))", "2")
+  , ("(defvar covid-19 2020)", "covid-19")
+  , ("(setq covid-19 2020)", "2020")
+  , ("(let () (defparameter x 1) x)", "1")
+  , ("(let () (setq y 'covid-19) y)", "covid-19")
   , ("(let ((x 1) (y 2) (z 3)) (+ x y z))", "6")
   , ("(let* ((x 1) (y (+ x 1)) (z (+ y 1))) (+ x y z))", "6")
-  , ("`(1 ,(+ 1 1) (- 4 1) 4)"         , "(1 2 (- 4 1) 4)")
+  , ("`(1 ,(+ 1 1) (- 4 1) 4)", "(1 2 (- 4 1) 4)")
   , ("(let ((x '(123)) (z '(7 8 9))) `(,@x 45 6 ,@z))", "(123 45 6 7 8 9)")
-  , ("(let ((x 3)) `(1 ,x \"foo\"))"   , "(1 3 \"foo\")")
-  , ("`(,@`(,@`(1 2 3) 112))"          , "(1 2 3 112)")
-  , ("`(,@'(+ 1 2))"                   , "(+ 1 2)")
-  , ("``(,@,`(1 2 3))"                 , "`(,@(1 2 3))")
-  , ("(eval `(list ,@'(9 5) 1 2 3))"   , "(9 5 1 2 3)")
-  , ("`(1 2 @,'(a b c))"               , "(1 2 @ (a b c))")
-  , ("(let ((x '(1 2 3))) `(,@x))"     , "(1 2 3)")
-  , ("(let ((x '(1 2 3))) ``(,@,x))"   , "`(,@(1 2 3))")
-  , ("``(a ,,(+ 1 2) ,(+ 3 4))"        , "`(a ,3 ,(+ 3 4))")
-  , ("``,,'(1 2)"                      , "`,(1 2)")
-  , ("`(,@`(1 2 3))"                   , "(1 2 3)")
-  , ("``(bq x ,x ,@x ,bq)"             , "`(bq x ,x ,@x ,bq)")
-  , ("`(,@'123)"                       , "123")
-  , ("(let ((y 123)) `(,@y))"          , "123")
-  , ("``(,@,@'(1 2 3))"                , "`(,@1 ,@2 ,@3)")
-  , ("(let ((x '(1 2 3))) ``(,,@x))"   , "`(,1 ,2 ,3)")
+  , ("(let ((x 3)) `(1 ,x \"foo\"))", "(1 3 \"foo\")")
+  , ("`(,@`(,@`(1 2 3) 112))", "(1 2 3 112)")
+  , ("`(,@'(+ 1 2))", "(+ 1 2)")
+  , ("``(,@,`(1 2 3))", "`(,@(1 2 3))")
+  , ("(eval `(list ,@'(9 5) 1 2 3))", "(9 5 1 2 3)")
+  , ("`(1 2 @,'(a b c))", "(1 2 @ (a b c))")
+  , ("(let ((x '(1 2 3))) `(,@x))", "(1 2 3)")
+  , ("(let ((x '(1 2 3))) ``(,@,x))", "`(,@(1 2 3))")
+  , ("``(a ,,(+ 1 2) ,(+ 3 4))", "`(a ,3 ,(+ 3 4))")
+  , ("``,,'(1 2)", "`,(1 2)")
+  , ("`(,@`(1 2 3))", "(1 2 3)")
+  , ("``(bq x ,x ,@x ,bq)", "`(bq x ,x ,@x ,bq)")
+  , ("`(,@'123)", "123")
+  , ("(let ((y 123)) `(,@y))", "123")
+  , ("``(,@,@'(1 2 3))", "`(,@1 ,@2 ,@3)")
+  , ("(let ((x '(1 2 3))) ``(,,@x))", "`(,1 ,2 ,3)")
   , ("``('a 'b ,@,@'(1 'c) () nil '())", "`('a 'b ,@1 ,@'c nil nil 'nil)")
-  , ( "(let ((x '(a b c))) `(x ,x ,@x foo ,(cadr x) bar ,(cdr x) baz ,@(cdr x)))"
+  ,
+    ( "(let ((x '(a b c))) `(x ,x ,@x foo ,(cadr x) bar ,(cdr x) baz ,@(cdr x)))"
     , "(x (a b c) a b c foo b bar (b c) baz b c)"
     )
-  , ( "(let ((c (list 22 33))) `(foobar a b ,c ,'(e f g) d ,@'(e f g) (h i j) ,@c))"
+  ,
+    ( "(let ((c (list 22 33))) `(foobar a b ,c ,'(e f g) d ,@'(e f g) (h i j) ,@c))"
     , "(foobar a b (22 33) (e f g) d e f g (h i j) 22 33)"
     )
-  , ("(append)"                            , "nil")
-  , ("(append 'a)"                         , "a")
-  , ("(append '(a b c) '(d e f) '() '(g))" , "(a b c d e f g)")
-  , ("(append '(a b c) 'd)"                , "(a b c . d)")
+  , ("(append)", "nil")
+  , ("(append 'a)", "a")
+  , ("(append '(a b c) '(d e f) '() '(g))", "(a b c d e f g)")
+  , ("(append '(a b c) 'd)", "(a b c . d)")
   , ("(let ((lst '(a b c))) (append lst '(d)))", "(a b c d)")
-  , ("(list)"                              , "nil")
-  , ("(when t)"                            , "nil")
-  , ("(when nil)"                          , "nil")
-  , ("(unless t)"                          , "nil")
-  , ("(unless nil)"                        , "nil")
+  , ("(list)", "nil")
+  , ("(when t)", "nil")
+  , ("(when nil)", "nil")
+  , ("(unless t)", "nil")
+  , ("(unless nil)", "nil")
   , ("(progn (setq lst '(1 2 3 4)) (butlast lst))", "(1 2 3)")
-  , ("(if nil 'true 'very-false)"          , "very-false")
-  , ("(let ((x 3)) (when (oddp x) 'odd))"  , "odd")
+  , ("(if nil 't 'very-false)", "very-false")
+  , ("(let ((x 3)) (when (oddp x) 'odd))", "odd")
   , ("(let ((x 3)) (unless (oddp x) 'odd))", "nil")
   , ("(let ((x 3)) (if (oddp (+ x x)) 'odd 'even))", "even")
   , ("(funcall '+ 2/3 4/5 6/7 8/9 (* 12/23 123/321))", "2645512/775215")
   , ("(progn (defun maria (x y) (/ x y)) (maria 1111 2222))", "1/2")
-  , ( "(progn (setq sofia (lambda (x y) (* x y))) (apply sofia 14 11 nil))"
+  ,
+    ( "(progn (setq sofia (lambda (x y) (* x y))) (apply sofia 14 11 nil))"
     , "154"
     )
   , ("((lambda (x y) (abs (/ (* #c(3 -3) x) y))) 2 #c(2 -2))", "3.0")
